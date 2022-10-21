caf568e9618f91fcec593aaf9c7fa19e3fac5b5d
sakilk130 <sakilk130@gmail.com>
2022-10-21 23:53:16 +0600

    feat: :sparkles: create header component

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,14 @@
+import { Box } from '@mui/material';
+import Header from '../../components/header';
+
 function DashboardPage() {
-  return <div>DashboardPage</div>;
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subTitle="Welcome to dashboard" />
+      </Box>
+    </Box>
+  );
 }
 
 export default DashboardPage;
